b562669c47482422bf639da23c07d57d87dc793e
Iveta <iveta.nagled@gmail.com>
2021-04-18 13:05:58 +0300

    DoggyController and repository

--- src/main/java/com/example/demo/controller/DoggyController.java
+++ src/main/java/com/example/demo/controller/DoggyController.java
@@ -1,4 +1,29 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Doggy;
+import com.example.demo.repository.DoggyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class DoggyController {
+
+    private final DoggyRepository doggyRepository;
+
+    @GetMapping("/all")
+    public List<Doggy> getAll() {
+        return doggyRepository.findAll();
+    }
+
+    @PostMapping("/save")
+    public String saveDoggy(@RequestBody Doggy doggy) {
+        doggyRepository.save(doggy);
+        return "Saved " + doggy.getName();
+    }
 }
--- src/main/java/com/example/demo/repository/DoggyRepository.java
+++ src/main/java/com/example/demo/repository/DoggyRepository.java
@@ -1,4 +1,13 @@
 package com.example.demo.repository;
 
-public class DoggyRepository {
+import com.example.demo.model.Doggy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DoggyRepository extends JpaRepository<Doggy, Integer> {
+
+    List<Doggy> findAllByName(String name);
 }
